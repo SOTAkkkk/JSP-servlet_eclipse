f1878fb843095ad6e137dc8b1d1cb42978663202
sota <sota@DESKTOP-7C9SHEJ>
2022-03-21 15:37:42 +0900

    servletでjavaのプログラムとhtmlを記述 これを参考に練習すべき

--- example/src/servlet/SampleServlet.java
+++ example/src/servlet/SampleServlet.java
@@ -2,6 +2,8 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,10 +17,27 @@ public class SampleServlet extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//運勢をランダムで決定
+		String[] luckArray = {"超スッキリ","スッキリ","悪夢"};
+		int index = (int)(Math.random()*3);
+		String luck = luckArray[index];
+		
+		//実行日を取得
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("MM月dd日");
+		String today = sdf.format(date);
+		
+		//HTMLを出力
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
-		out.println("...");
+		out.println("<head>");
+		out.println("<title>スッキリ占い</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<p>"+today+"の運勢は「"+luck+"」です</p>");
+		out.println("</body>");
 		out.println("</html>");//コメント
 	}
 
